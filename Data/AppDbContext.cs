a417f00dcbe677d27077e862a43aeb133bdc647c
joaozamarin <joao.zamarin@gmail.com>
2023-04-22 11:18:46 -0300

    criação usuário cliente e administrador

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using CentroAutomotivo.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,7 +8,7 @@ namespace CentroAutomotivo.Data
     public class AppDbContext : IdentityDbContext<AppUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {            
+        {
         }
 
         public DbSet<AppUser> AppUsers { get; set; }
@@ -33,7 +34,7 @@ namespace CentroAutomotivo.Data
                 .HasOne(so => so.OrdemServico)
                 .WithMany(so => so.ServicosOrdem)
                 .HasForeignKey(so => so.OrdemServicoId);
-            
+
             builder.Entity<ServicoOrdem>()
                 .HasOne(so => so.Servico)
                 .WithMany(so => so.ServicosOrdem)
@@ -43,16 +44,92 @@ namespace CentroAutomotivo.Data
             #region N : N - PecaOrdem
             builder.Entity<PecaOrdem>()
                 .HasKey(po => new { po.OrdemServicoId, po.PecaId });
-            
+
             builder.Entity<PecaOrdem>()
                 .HasOne(po => po.OrdemServico)
                 .WithMany(po => po.PecasOrdem)
                 .HasForeignKey(po => po.OrdemServicoId);
-            
+
             builder.Entity<PecaOrdem>()
                 .HasOne(po => po.Peca)
                 .WithMany(po => po.PecasOrdem)
                 .HasForeignKey(po => po.PecaId);
+            #endregion
+
+            #region Seed Roles
+            List<IdentityRole> listRoles = new()
+            {
+                new IdentityRole{
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Administrador",
+                    NormalizedName = "ADMINISTRADOR"
+                },
+
+                new IdentityRole{
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Usuário",
+                    NormalizedName = "USUÁRIO"
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(listRoles);
+            #endregion
+
+            #region Seed ApplicationUser - Admin
+            var userId = Guid.NewGuid().ToString();
+            var hash = new PasswordHasher<AppUser>();
+            builder.Entity<AppUser>().HasData(
+                new AppUser
+                {
+                    Id = userId,
+                    Nome = "Administrador",
+                    UserName = "admin@javacentroautomotivo.com",
+                    NormalizedUserName = "ADMIN@JAVACENTROAUTOMOTIVO.COM",
+                    Email = "admin@javacentroautomotivo.com",
+                    NormalizedEmail = "ADMIN@JAVACENTROAUTOMOTIVO.COM",
+                    EmailConfirmed = true,
+                    PasswordHash = hash.HashPassword(null, "admin"),
+                    SecurityStamp = hash.GetHashCode().ToString(),
+                    FotoPerfil = @"\avatars\01.png",
+                    IsAdmin = true
+                }
+            );
+
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    UserId = userId,
+                    RoleId = listRoles[0].Id
+                }
+            );
+            #endregion
+
+            #region Seed ApplicationUser - Usuário
+            var userClienteId = Guid.NewGuid().ToString();
+            var hashCliente = new PasswordHasher<AppUser>();
+            builder.Entity<AppUser>().HasData(
+                new AppUser
+                {
+                    Id = userClienteId,
+                    Nome = "Cliente",
+                    UserName = "cliente@javacentroautomotivo.com",
+                    NormalizedUserName = "CLIENTE@JAVACENTROAUTOMOTIVO.COM",
+                    Email = "cliente@javacentroautomotivo.com",
+                    NormalizedEmail = "CLIENTE@JAVACENTROAUTOMOTIVO.COM",
+                    EmailConfirmed = true,
+                    PasswordHash = hashCliente.HashPassword(null, "cliente"),
+                    SecurityStamp = hashCliente.GetHashCode().ToString(),
+                    FotoPerfil = @"\avatars\06.png",
+                    IsAdmin = false
+                }
+            );
+
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    UserId = userClienteId,
+                    RoleId = listRoles[1].Id
+                }
+            );
             #endregion
         }
     }
---
+++ Migrations/20230422141533_SeedUsers.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CentroAutomotivo.Migrations
+{
+    public partial class SeedUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "947b775b-e4b2-483e-8492-8744dff12e2d", "22973331-b968-4ea9-99c0-f971ddb31d66", "Administrador", "ADMINISTRADOR" },
+                    { "fc40412e-eaa8-4c04-81c9-5c7d399c13cb", "70d0fc1f-3d5a-456d-b803-5547f7bfadec", "Usuário", "USUÁRIO" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FotoPerfil", "IsAdmin", "LockoutEnabled", "LockoutEnd", "Nome", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "94e45df8-48cd-47a8-9d9e-ceb096b6b40a", 0, "46ab7d4c-ea90-4cd1-90d4-91df419f2887", "admin@javacentroautomotivo.com", true, "\\avatars\\01.png", true, false, null, "Administrador", "ADMIN@JAVACENTROAUTOMOTIVO.COM", "ADMIN@JAVACENTROAUTOMOTIVO.COM", "AQAAAAEAACcQAAAAECTWkIUNdEkwa5aCqxS7YgMmFEWMRecl+QrAgw3wlRmpX74xsWWJyqG+in3iLS5XbQ==", null, false, "1807185", false, "admin@javacentroautomotivo.com" },
+                    { "afb4c13c-b8fc-4496-ba30-c1a3491122f1", 0, "fb60fc31-8459-4d5f-8b2f-cf787378313f", "cliente@javacentroautomotivo.com", true, "\\avatars\\06.png", false, false, null, "Cliente", "CLIENTE@JAVACENTROAUTOMOTIVO.COM", "CLIENTE@JAVACENTROAUTOMOTIVO.COM", "AQAAAAEAACcQAAAAEMXLygp6sSX4XvctFGGLc23qHHhl396Jl1+vpfa+ZSD6qDimePq9oqiVtBkSMCXPFA==", null, false, "16264673", false, "cliente@javacentroautomotivo.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "947b775b-e4b2-483e-8492-8744dff12e2d", "94e45df8-48cd-47a8-9d9e-ceb096b6b40a" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "fc40412e-eaa8-4c04-81c9-5c7d399c13cb", "afb4c13c-b8fc-4496-ba30-c1a3491122f1" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "947b775b-e4b2-483e-8492-8744dff12e2d", "94e45df8-48cd-47a8-9d9e-ceb096b6b40a" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "fc40412e-eaa8-4c04-81c9-5c7d399c13cb", "afb4c13c-b8fc-4496-ba30-c1a3491122f1" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "947b775b-e4b2-483e-8492-8744dff12e2d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fc40412e-eaa8-4c04-81c9-5c7d399c13cb");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "94e45df8-48cd-47a8-9d9e-ceb096b6b40a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "afb4c13c-b8fc-4496-ba30-c1a3491122f1");
+        }
+    }
+}
