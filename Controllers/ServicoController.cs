3ee9499bef8fe26ef6f59b8c15489cf064212247
joaozamarin <joao.zamarin@gmail.com>
2023-05-03 17:28:01 -0300

    crud serviços área administrativa e alterações no layout

---
+++ Controllers/ServicoController.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CentroAutomotivo.Data;
+using CentroAutomotivo.Models;
+
+namespace CentroAutomotivo.Controllers
+{
+    public class ServicoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ServicoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Servico
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Servicos.ToListAsync());
+        }
+
+        // GET: Servico/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Servicos == null)
+            {
+                return NotFound();
+            }
+
+            var servico = await _context.Servicos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+
+            return View(servico);
+        }
+
+        // GET: Servico/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Servico/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Preco")] Servico servico)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(servico);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servico);
+        }
+
+        // GET: Servico/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Servicos == null)
+            {
+                return NotFound();
+            }
+
+            var servico = await _context.Servicos.FindAsync(id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+            return View(servico);
+        }
+
+        // POST: Servico/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Preco")] Servico servico)
+        {
+            if (id != servico.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(servico);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServicoExists(servico.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servico);
+        }
+
+        // GET: Servico/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Servicos == null)
+            {
+                return NotFound();
+            }
+
+            var servico = await _context.Servicos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+
+            return View(servico);
+        }
+
+        // POST: Servico/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Servicos == null)
+            {
+                return Problem("Entity set 'AppDbContext.Servicos'  is null.");
+            }
+            var servico = await _context.Servicos.FindAsync(id);
+            if (servico != null)
+            {
+                _context.Servicos.Remove(servico);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServicoExists(int id)
+        {
+          return _context.Servicos.Any(e => e.Id == id);
+        }
+    }
+}
