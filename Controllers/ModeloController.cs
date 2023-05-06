6cf121a916987531e4ef04db611343fe8b28cb31
joaozamarin <joao.zamarin@gmail.com>
2023-05-06 13:23:14 -0300

    crud modelos area admin

---
+++ Controllers/ModeloController.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CentroAutomotivo.Data;
+using CentroAutomotivo.Models;
+
+namespace CentroAutomotivo.Controllers
+{
+    public class ModeloController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ModeloController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Modelo
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Modelos.Include(m => m.Marca);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Modelo/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Modelos == null)
+            {
+                return NotFound();
+            }
+
+            var modelo = await _context.Modelos
+                .Include(m => m.Marca)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (modelo == null)
+            {
+                return NotFound();
+            }
+
+            return View(modelo);
+        }
+
+        // GET: Modelo/Create
+        public IActionResult Create()
+        {
+            ViewData["MarcaId"] = new SelectList(_context.Marcas, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Modelo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Ano,MarcaId")] Modelo modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(modelo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MarcaId"] = new SelectList(_context.Marcas, "Id", "Nome", modelo.MarcaId);
+            return View(modelo);
+        }
+
+        // GET: Modelo/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Modelos == null)
+            {
+                return NotFound();
+            }
+
+            var modelo = await _context.Modelos.FindAsync(id);
+            if (modelo == null)
+            {
+                return NotFound();
+            }
+            ViewData["MarcaId"] = new SelectList(_context.Marcas, "Id", "Nome", modelo.MarcaId);
+            return View(modelo);
+        }
+
+        // POST: Modelo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Ano,MarcaId")] Modelo modelo)
+        {
+            if (id != modelo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(modelo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ModeloExists(modelo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MarcaId"] = new SelectList(_context.Marcas, "Id", "Nome", modelo.MarcaId);
+            return View(modelo);
+        }
+
+        // GET: Modelo/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Modelos == null)
+            {
+                return NotFound();
+            }
+
+            var modelo = await _context.Modelos
+                .Include(m => m.Marca)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (modelo == null)
+            {
+                return NotFound();
+            }
+
+            return View(modelo);
+        }
+
+        // POST: Modelo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Modelos == null)
+            {
+                return Problem("Entity set 'AppDbContext.Modelos'  is null.");
+            }
+            var modelo = await _context.Modelos.FindAsync(id);
+            if (modelo != null)
+            {
+                _context.Modelos.Remove(modelo);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ModeloExists(int id)
+        {
+          return _context.Modelos.Any(e => e.Id == id);
+        }
+    }
+}
