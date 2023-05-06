cb9e165c9624fbff21f281f631c2db6e331fe7e9
joaozamarin <joao.zamarin@gmail.com>
2023-05-06 11:38:52 -0300

    crud status area admin

---
+++ Controllers/StatusOrdemServicoController.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CentroAutomotivo.Data;
+using CentroAutomotivo.Models;
+
+namespace CentroAutomotivo.Controllers
+{
+    public class StatusOrdemServicoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StatusOrdemServicoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: StatusOrdemServico
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.StatusOrdensServico.ToListAsync());
+        }
+
+        // GET: StatusOrdemServico/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.StatusOrdensServico == null)
+            {
+                return NotFound();
+            }
+
+            var statusOrdemServico = await _context.StatusOrdensServico
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (statusOrdemServico == null)
+            {
+                return NotFound();
+            }
+
+            return View(statusOrdemServico);
+        }
+
+        // GET: StatusOrdemServico/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StatusOrdemServico/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Cor")] StatusOrdemServico statusOrdemServico)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(statusOrdemServico);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(statusOrdemServico);
+        }
+
+        // GET: StatusOrdemServico/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.StatusOrdensServico == null)
+            {
+                return NotFound();
+            }
+
+            var statusOrdemServico = await _context.StatusOrdensServico.FindAsync(id);
+            if (statusOrdemServico == null)
+            {
+                return NotFound();
+            }
+            return View(statusOrdemServico);
+        }
+
+        // POST: StatusOrdemServico/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Cor")] StatusOrdemServico statusOrdemServico)
+        {
+            if (id != statusOrdemServico.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(statusOrdemServico);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StatusOrdemServicoExists(statusOrdemServico.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(statusOrdemServico);
+        }
+
+        // GET: StatusOrdemServico/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.StatusOrdensServico == null)
+            {
+                return NotFound();
+            }
+
+            var statusOrdemServico = await _context.StatusOrdensServico
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (statusOrdemServico == null)
+            {
+                return NotFound();
+            }
+
+            return View(statusOrdemServico);
+        }
+
+        // POST: StatusOrdemServico/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.StatusOrdensServico == null)
+            {
+                return Problem("Entity set 'AppDbContext.StatusOrdensServico'  is null.");
+            }
+            var statusOrdemServico = await _context.StatusOrdensServico.FindAsync(id);
+            if (statusOrdemServico != null)
+            {
+                _context.StatusOrdensServico.Remove(statusOrdemServico);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StatusOrdemServicoExists(int id)
+        {
+          return _context.StatusOrdensServico.Any(e => e.Id == id);
+        }
+    }
+}
